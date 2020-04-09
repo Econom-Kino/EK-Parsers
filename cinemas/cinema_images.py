45bf3c59387de0957161e984f3c27e457e1954ef
Yaroslav Kukhar <kukhar.yr@gmail.com>
2020-04-09 20:02:50 +0300

    add deleting images

--- cinemas/cinema_images.py
+++ cinemas/cinema_images.py
@@ -1,15 +1,42 @@
 import requests
 
 from keys import GOOGLE_API_KEY
-from const import EK_CINEMA_IMAGES_API, CINEMA_PHOTOS_API, EK_CINEMAS_API, CINEMA_DETAILS_API
+from const import EK_CINEMA_IMAGES_API, CINEMA_PHOTOS_API, CINEMA_DETAILS_API
+
+
+# POST запит до EK API для додавання нових фото
+def post_cinema_photos(img_to_post, place_id):
+    for img in img_to_post:
+        data = {
+            'cinema': place_id,
+            'image_link': img
+        }
+        post = requests.post(EK_CINEMA_IMAGES_API, data=data)
+        print('Images POST request:', post.status_code)
+    print()
+
+
+def delete_local_cinema_photos(num_to_delete, place_id):
+    local_cinema_photos_request = EK_CINEMA_IMAGES_API + 'cinema/' + place_id + '/'
+
+    for count, photo in enumerate(requests.get(local_cinema_photos_request).json()):
+        if count == num_to_delete:
+            break
+
+        photo_to_del_request = EK_CINEMA_IMAGES_API + 'id/' + str(photo['id']) + '/'
+        delete = requests.delete(photo_to_del_request)
+
+        print("delete_local_cinema_photos: ", delete)
 
 
 # Функція повертає сет з силками на фото кінотеатрів, що є у нашій базі
-def get_local_cinema_photos():
+def get_local_cinema_photos(place_id):
     local_cinema_photos = set()
+    local_cinema_photos_request = EK_CINEMA_IMAGES_API + 'cinema/' + place_id + '/'
 
-    for link in requests.get(EK_CINEMA_IMAGES_API).json():
-        local_cinema_photos.add(link['image_link'])
+    for photo in requests.get(local_cinema_photos_request).json():
+        local_cinema_photos.add(photo['image_link'])
+
     return local_cinema_photos
 
 
@@ -18,11 +45,13 @@ def get_local_cinema_photos():
 def get_cinema_photos(photo_references):
     parsed_photos = set()
 
-    for photo in photo_references:
+    for count, photo in enumerate(photo_references):
         cinema_photo_request = CINEMA_PHOTOS_API + photo['photo_reference'] + '&key=' + GOOGLE_API_KEY
-
         photo_link = requests.get(cinema_photo_request)
         parsed_photos.add(photo_link.url)
+
+        if count == 5:
+            break
 
     return parsed_photos
 
@@ -31,26 +60,21 @@ def get_cinema_photos(photo_references):
 # @param: place_ids - список кінотеатрів
 def update_cinema_images(place_ids):
     for place_id in place_ids:
-        # GET запит до EK API для отримання id поточного кінотеатру
-        cinema_request = EK_CINEMAS_API + place_id + '/'
-        cinema = requests.get(cinema_request)
-
         # GET запит до  GOOGLE API для отроимання ключів фотографій (photo_references)
         photo_references_request = CINEMA_DETAILS_API + 'place_id=' + place_id + '&fields=photos&key=' + GOOGLE_API_KEY
         photo_references = requests.get(photo_references_request).json()['result']['photos']
 
-        # POST запит до EK API для додавання нових фото
-        if cinema.status_code == 200:
-            img_to_post = get_cinema_photos(photo_references) - get_local_cinema_photos()
-            # print("Images to POST:", img_to_post)
+        local_cinema_photos = get_local_cinema_photos(place_id)
 
-            for img in img_to_post:
-                data = {
-                    'cinema': cinema.json()['id'],
-                    'image_link': img
-                }
-                post = requests.post(EK_CINEMA_IMAGES_API, data=data)
-                print(cinema.json()['name'] + ': Images POST request:', post.status_code)
+        if len(local_cinema_photos) == 6:
+            img_to_post = get_cinema_photos(photo_references) - local_cinema_photos
+            print("Images to POST:", img_to_post)
+
+            delete_local_cinema_photos(len(img_to_post), place_id)
+            post_cinema_photos(img_to_post, place_id)
         else:
-            print("update_cinema_images: Something goes ne tak")
-        print()
+            img_to_post = get_cinema_photos(photo_references)
+            print("Images to POST:", img_to_post)
+
+            delete_local_cinema_photos(len(local_cinema_photos), place_id)
+            post_cinema_photos(img_to_post, place_id)

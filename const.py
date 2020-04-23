a8c8200d677b07e91774b690a618ebda2f15a246
Yaroslav Kukhar <kukhar.yr@gmail.com>
2020-04-23 16:55:50 +0300

    add fake session generator

--- const.py
+++ const.py
@@ -2,15 +2,15 @@ from keys import GOOGLE_API_KEY, TMDB_KEY
 
 
 # Cinemas
-# EK_CINEMAS_API = 'https://ekinoback.herokuapp.com/cinemas/'
-EK_CINEMAS_API = 'http://127.0.0.1:8000/cinemas/'
+EK_CINEMAS_API = 'https://ekinoback.herokuapp.com/cinemas/'
+# EK_CINEMAS_API = 'http://127.0.0.1:8000/cinemas/'
 CINEMA_DETAILS_API = 'https://maps.googleapis.com/maps/api/place/details/json?language=uk&'
 CINEMA_DETAILS_FIELDS = 'name,rating,formatted_phone_number,place_id,website,formatted_address,geometry'
 CINEMA_DETAILS_REQUEST = CINEMA_DETAILS_API + 'fields=' + CINEMA_DETAILS_FIELDS + '&key=' + GOOGLE_API_KEY
 
 # Cinema photos
-# EK_CINEMA_IMAGES_API = 'https://ekinoback.herokuapp.com/cinema-images/'
-EK_CINEMA_IMAGES_API = 'http://127.0.0.1:8000/cinema-images/'
+EK_CINEMA_IMAGES_API = 'https://ekinoback.herokuapp.com/cinema-images/'
+# EK_CINEMA_IMAGES_API = 'http://127.0.0.1:8000/cinema-images/'
 CINEMA_PHOTOS_API = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photoreference='
 
 # Genres
---
+++ sessions/fake_sessions.py
@@ -0,0 +1,38 @@
+import datetime
+from random import randint
+
+import requests
+
+TECHNOLOGIES = ['2D', '3D', '4DX']
+place_ids = open('../cinemas/cinema_place_ids.txt', 'r').read().split('\n')
+
+
+def generate_rand_sessions_for_movie(movie_id):
+    sessions = []
+    for day in range(7):
+        for _ in range(randint(10, 15)):
+            sessions.append({
+                'cinema': place_ids[randint(0, 5)],
+                'movie': movie_id,
+                'price': randint(10, 25) * 5,
+                'start_time': str(datetime.date.today() + datetime.timedelta(days=day)) + "T" + str(randint(8, 23)) + ":00:00+03:00",
+                "ticket_link": "https://www.google.com",
+                "language": "Українська",
+                "technology": TECHNOLOGIES[randint(0, 2)]
+            })
+    return sessions
+
+
+def post_fake_sessions():
+    movies_in_rolling = requests.get('https://ekinoback.herokuapp.com/movies/inRolling/')
+
+    for movie in movies_in_rolling.json():
+        sessions = generate_rand_sessions_for_movie(movie['id'])
+        for session in sessions:
+            print(session)
+            post = requests.post('https://ekinoback.herokuapp.com/sessions/', data=session)
+            print(post)
+
+
+if __name__ == '__main__':
+    post_fake_sessions()

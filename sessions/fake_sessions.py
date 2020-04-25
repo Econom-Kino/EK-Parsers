2821521a4203384c661ceb47df7bba77ed300d9f
Yaroslav Kukhar <kukhar.yr@gmail.com>
2020-04-25 19:27:01 +0300

    added posting list of sessions

--- sessions/fake_sessions.py
+++ sessions/fake_sessions.py
@@ -13,12 +13,12 @@ place_ids = open('../cinemas/cinema_place_ids.txt', 'r').read().split('\n')
 def generate_rand_sessions_for_movie(movie_id, day_from, num_of_days):
     sessions = []
     for day in range(day_from, day_from+num_of_days):
-        for _ in range(randint(10, 15)):
+        for _ in range(randint(10, 14)):
             sessions.append({
-                'cinema': place_ids[randint(0, 5)],
-                'movie': movie_id,
-                'price': randint(10, 25) * 5,
-                'start_time': str(datetime.date.today() + datetime.timedelta(days=day)) + "T" + str(randint(8, 23)) + ":00:00+03:00",
+                "cinema": place_ids[randint(0, 5)],
+                "movie": movie_id,
+                "price": randint(10, 25) * 5,
+                "start_time": str(datetime.date.today() + datetime.timedelta(days=day)) + "T" + str(randint(8, 23)) + ":00:00+03:00",
                 "ticket_link": "https://www.google.com",
                 "language": "Українська",
                 "technology": TECHNOLOGIES[randint(0, 2)]
@@ -33,9 +33,11 @@ def post_fake_sessions(day_from, num_of_days):
         sessions = generate_rand_sessions_for_movie(movie['id'], day_from, num_of_days)
         for session in sessions:
             print(session)
-            post = requests.post(EK_SESSIONS_API, data=session)
-            print(post)
+
+        post = requests.post(EK_SESSIONS_API, json=sessions)
+        print(post)
+        print()
 
 
 if __name__ == '__main__':
-    post_fake_sessions(day_from=1, num_of_days=7)
+    post_fake_sessions(day_from=0, num_of_days=7)

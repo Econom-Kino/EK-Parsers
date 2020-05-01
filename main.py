37312b6d103b0a0ec1e6558cc7ec062765f97d40
Yaroslav Kukhar <kukhar.yr@gmail.com>
2020-05-01 22:40:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Made by EK-Team
-from logs import write_log
 from cinemas.update_cinemas import update_cinemas
 from genres.update_genres import update_genres
 from movies.update_movies import update_movies
@@ -8,33 +7,17 @@ from sessions.fake_sessions import post_fake_sessions
 
 def main():
     # Cinemas
-    try:
-        update_cinemas()
-    except:
-        write_log("update_cinemas() error")
-        print("update_cinemas() error")
+    update_cinemas()
 
     # Genres
-    try:
-        update_genres()
-    except:
-        write_log("update_cinemas() error")
-        print("update_cinemas() error")
+    update_genres()
 
     # Movies
-    try:
-        update_movies()
-    except:
-        write_log("update_movies() error")
-        print("update_movies() error")
+    update_movies()
 
     # Sessions
-    try:
-        # post_fake_sessions("week")
-        post_fake_sessions(adding_type="day")
-    except:
-        write_log("post_fake_sessions() error")
-        print("post_fake_sessions() error")
+    # post_fake_sessions("week")
+    post_fake_sessions(adding_type="day")
 
 
 if __name__ == '__main__':
